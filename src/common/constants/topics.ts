eb24ee7b1f0f8b38737bede8068443c360199fba
RomanStass <roman.stasianok@cbitech.ae>
2022-11-08 09:39:46 +0300

    Init

---
+++ src/common/constants/topics.ts
@@ -0,0 +1,8 @@
+export const TOPIC_AUTH_REGISTER = 'auth.register';
+export const TOPIC_AUTH_REGISTER_REPLY = 'auth.register.reply';
+
+export const TOPIC_USER_CREATE = 'user.create';
+
+export const TOPIC_USER_UPDATE = 'user.update';
+
+export const TOPIC_USER_REMOVE = 'user.remove';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { v4 as uuidv4 } from 'uuid';
+import { ExceptionFilter } from './exceptions/rpc-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      {
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:9092'],
+            clientId: 'mailer-service',
+          },
+          consumer: {
+            groupId: `mailer-consumer-${uuidv4()}`,
+            allowAutoTopicCreation: true,
+          },
+        },
+      },
+  );
+
+  app.useGlobalFilters(new ExceptionFilter());
+
+  app.enableShutdownHooks();
+
+  await app.listen();
 }
-bootstrap();
+bootstrap().then(() => {
+    console.log('Mailer service is running');
+});
